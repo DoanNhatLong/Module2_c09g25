0a29e56c804d023f4a166c69e0506a1513a670a2
DoanNhatLong <longs3515@gmail.com>
2025-11-13 11:05:57 +0700

    bai_tap

---
+++ src/ss7_interface/bai_tap/bai1/Main.java
@@ -0,0 +1,10 @@
+package ss7_interface.bai_tap.bai1;
+
+import ss6_inheritance.thuc_hanh.hinh_hoc.Circle;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle test=new Circle(10);
+        test.resizeShape(30);
+    }
+}
---
+++ src/ss7_interface/bai_tap/bai1/Resizeable.java
@@ -0,0 +1,5 @@
+package ss7_interface.bai_tap.bai1;
+
+public interface Resizeable {
+    void resizeShape(double percent);
+}
