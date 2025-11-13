ce0c69568ef30b9d3ef16bd167ff3c9758f700a8
DoanNhatLong <longs3515@gmail.com>
2025-11-13 11:06:50 +0700

    bai_tap

---
+++ src/ss7_interface/bai_tap/bai2/Main.java
@@ -0,0 +1,11 @@
+package ss7_interface.bai_tap.bai2;
+
+import ss6_inheritance.thuc_hanh.hinh_hoc.Square;
+
+public class Main {
+    public static void main(String[] args) {
+        Square test=new Square(10);
+        test.resizeShape(50);
+        test.howToColor();
+    }
+}
